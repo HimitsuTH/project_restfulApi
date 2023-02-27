16d250d6623b3dd838ac66cc035bbc45b08c60ed
Chinnawich <hotlnw159@gmail.com>
2023-02-27 14:31:03 +0700

    update usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,7 @@ exports.register = async (req, res, next) => {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      const error = new Error("received incorrect information!");
+      const error = new Error("received incorrect information ❗");
       error.statusCode = 422;
       error.validation = errors.array();
       throw error;
@@ -25,7 +25,7 @@ exports.register = async (req, res, next) => {
     const existEmail = await User.findOne({ email: email });
 
     if (existEmail) {
-      const error = new Error("Email has alrealy exist!");
+      const error = new Error("Email has alrealy exist ❗");
       error.statusCode = 400;
       throw error;
     }
@@ -52,7 +52,7 @@ exports.login = async (req, res, next) => {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      const error = new Error("received incorrect information!");
+      const error = new Error("received incorrect information ❗");
       error.statusCode = 422;
       error.validation = errors.array();
       throw error;
@@ -60,14 +60,14 @@ exports.login = async (req, res, next) => {
 
     const user = await User.findOne({ email: email });
     if (!user) {
-      const error = new Error("User has alrealy exist!");
+      const error = new Error("User has alrealy exist ❗");
       error.statusCode = 404;
       throw error;
     }
 
     const isValid = await user.checkPassword(password);
     if (!isValid) {
-      const error = new Error("password not match!");
+      const error = new Error("password not match ❗");
       error.statusCode = 401;
       throw error;
     }
