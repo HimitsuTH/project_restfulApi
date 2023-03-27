d39a1b6c824c62fba65e4d5bd7dcfa538605267b
chinnawich <hotlnw159@gmail.com>
2023-03-27 21:31:27 +0700

    Test

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const userRouter = require("@routes/user");
 // const brandRouter = require("./routes/brand")
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:4000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Methods",
     "POST, GET, PUT, PATCH, DELETE, OPTIONS"
